d84bc19f9a2b3c6047a4d6115c0339ffe5f1772d
Eder Luiz <160525399+Luiz-Eder@users.noreply.github.com>
2025-10-28 09:05:47 -0300

    up
    Refactor ITaskService to use TaskEntity instead of System.Threading.Tasks.Task.

--- Priorix/Core/Interfaces/IServices/ITaskService.cs
+++ Priorix/Core/Interfaces/IServices/ITaskService.cs
@@ -1,14 +1,17 @@
-﻿using System.Collections.Generic;
+using System.Collections.Generic;
 using Priorix.Core.Entities;
+using TaskEntity = Priorix.Core.Entities.Task;
 
 namespace Priorix.Core.Interfaces.Services
 {
     public interface ITaskService
     {
-        IEnumerable<System.Threading.Tasks.Task> GetAllTasks();
-        System.Threading.Tasks.Task GetTaskById(int id);
+        IEnumerable<TaskEntity> GetAllTasks();
+        TaskEntity GetTaskById(int id);
+        void CreateTask(TaskEntity task);
+        void UpdateTask(TaskEntity task);
+        void DeleteTask(int id);
         void CreateTask(System.Threading.Tasks.Task task);
         void UpdateTask(System.Threading.Tasks.Task task);
-        void DeleteTask(int id);
     }
 }
