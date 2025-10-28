090d9f28ccc78effbe5d786f265a571b7efbfa93
Eder Luiz <160525399+Luiz-Eder@users.noreply.github.com>
2025-10-28 19:57:34 -0300

    Refactor IPriorizationService interface comments

--- Priorix/Core/Interfaces/IServices/IPriorizationService.cs
+++ Priorix/Core/Interfaces/IServices/IPriorizationService.cs
@@ -1,15 +1,13 @@
 ﻿using System.Collections.Generic;
 using Priorix.Core.Entities;
-using Priorix.Application.Dtos; // DTO que você criou
+using Priorix.Application.Dtos; 
 
 namespace Priorix.Core.Interfaces.Services
 {
     public interface IPriorizationService
     {
-        // Corrigido: agora usa a classe existente 'PriorizationMetrics'
         PriorizationMetrics CalculateAndSaveMetrics(RiceInputDto input);
 
-        // Retorna a lista de tarefas priorizadas
         IEnumerable<System.Threading.Tasks.Task> GetPrioritizedTasks();
     }
 }
