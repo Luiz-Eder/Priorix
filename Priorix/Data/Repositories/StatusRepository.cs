86e7c175703cc3b2f8049637030efed659055a09
Eder Luiz <160525399+Luiz-Eder@users.noreply.github.com>
2025-11-04 20:39:12 -0300

    Update

--- Priorix/Data/Repositories/StatusRepository.cs
+++ Priorix/Data/Repositories/StatusRepository.cs
@@ -1,7 +1,7 @@
-﻿using Priorix.Core.Entities;
+using Priorix.Core.Entities;
 using Priorix.Core.Interfaces.Repositories;
-using Priorix.Core.Services;
 using Priorix.Data.Context;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 
@@ -50,17 +50,27 @@ namespace Priorix.Data.Repositories
 
         public void DeleteStatus(int id, object v)
         {
-            throw new NotImplementedException();
+            var status = _db.Status.FirstOrDefault(s => s.Id == id);
+            if (status != null)
+            {
+                _db.Status.Remove(status);
+                _db.SaveChanges();
+            }
         }
 
         public List<Status> GetStatus()
         {
-            throw new NotImplementedException();
+            return _db.Status.ToList();
         }
 
         public object GetV()
         {
-            throw new NotImplementedException();
+            // Exemplo: retornar contagem total de status
+            return new
+            {
+                Total = _db.Status.Count(),
+                UltimoStatus = _db.Status.OrderByDescending(s => s.Id).FirstOrDefault()
+            };
         }
     }
 }
